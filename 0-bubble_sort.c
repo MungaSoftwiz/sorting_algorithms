74f470a69b38fd79ade5526957cc819fd2c7fd25
MungaSoftwiz <mwachilumobm@gmail.com>
2023-11-20 14:08:53 +0300

    Sort in ascending order using bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array using the algorithm
+ * @array: the array to sort
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t pass, index, temp;
+	bool swapped = true;
+
+	if (array == NULL && size < 2)
+		return;
+
+	for (pass = size - 1; pass > 0 && swapped; pass--)
+	{
+		swapped = false;
+
+		for (index = 0; index <= pass - 1; index++)
+		{
+			if (array[index] > array[index + 1])
+			{
+				/* swap elements and update swapped */
+				temp = array[index];
+				array[index] = array[index + 1];
+				array[index + 1] = temp;
+
+				print_array(array, size);
+				swapped = true;
+			}
+		}
+	}
+}
